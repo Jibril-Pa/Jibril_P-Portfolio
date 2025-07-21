f273345b9844087da90a9b52f359553014663fd4
Jibril-Pa <109011292+Jibril-Pa@users.noreply.github.com>
2025-07-20 23:06:46 -0700

    Added Things to Organizations Page

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -4,7 +4,7 @@ import DefaultLayout from "@/layouts/default";
 export default function ExperiencePage() {
   return (
     <DefaultLayout>
-      <section className="flex flex-col items-center gap-8 py-8 md:py-10">
+      <section className="flex flex-col items-center gap-8 py-8 md:pt-2">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Work Experience</h1>
         </div>
@@ -18,14 +18,14 @@ export default function ExperiencePage() {
               <span className="text-sm text-muted-foreground">May 2024 - July 2024</span>
             </div>
             <ul className="list-disc list-inside space-y-2 text-foreground">
-              <li>Created a database with over 77,000 IOC in excel with the use of data in Zerofox, Proofpoint, and Mandiant which will be used by the DSOC team to analyze threats to HEB systems.</li>
-              <li>Led a group of interns in a conjoined project that automates a ticketing system that takes data from Splunk to Jira.</li>
+              <li>Created a database with over <span className="font-bold text-primary text-blue-600">77,000 IOC</span> in excel with the use of data in Zerofox, Proofpoint, and Mandiant which will be used by the DSOC team to analyze threats to HEB systems.</li>
+              <li>Led a group of interns <span className="font-bold text-primary text-blue-600">10</span> interns in a conjoined project that automates a ticketing system that takes data from Splunk to Jira.</li>
               <li>Utilized the Feedly AI to create weekly newsletters regarding topics happening in cyber and new critical vulnerabilities and threats that could make HEB systems vulnerable to attack.</li>
               <li>Collaborated with personnel at Mandiant to ensure the digital footprint of partners are safe and no mentions of compromised data are found in the dark web.</li>
-              <li>Remediated over 50 devices through 5 different stores during the CrowdStrike incident of 2024</li>
+              <li>Remediated over <span className="font-bold text-primary text-blue-600">50</span> devices through <span className="font-bold text-primary text-blue-600">5</span> different stores during the CrowdStrike incident of <span className="font-bold text-primary text-blue-600">2024</span></li>
               <li>Developed and automated Python code to transport container image data from AWS cloud services to the Brinqa Risk Management platform for future use in Vulnerability management.</li>
-              <li>Utilized the Black Kite Risk Management platform to quickly analyze and notify teams on newly found vulnerabilities</li>
-              <li>Partnered with over 10 digital security teams to address high-critical vulnerabilities in HEB systems</li>
+              <li>Utilized the Black Kite Risk Management platform to quickly analyze and notify teams on <span className="font-bold text-primary text-blue-600">hundreds</span> of newly found vulnerabilities</li>
+              <li>Partnered with over <span className="font-bold text-primary text-blue-600">10</span> digital security teams to address high-critical vulnerabilities in HEB systems</li>
               <li>Documented automation within the confluence platform for reference and future use on new data integrations.</li>
             </ul>
           </div>
@@ -39,12 +39,12 @@ export default function ExperiencePage() {
               <span className="text-sm text-muted-foreground">May 2022 - Aug 2022</span>
             </div>
             <ul className="list-disc list-inside space-y-2 text-foreground">
-              <li>Updated laptop/desktop operating systems for students to use for the upcoming school year.</li>
+              <li>Updated over <span className="font-bold text-primary text-blue-600">500 laptop/desktop operating systems</span>  for students to use for the upcoming school year.</li>
               <li>Ensured that damaged technology was restored or recycled based on condition.</li>
-              <li>Assembled newer desktops for terchnical classes to use for the upcoming school year.</li>
-              <li>Installed security software on new laptops and desktops to keep user information private.</li>
+              <li>Assembled <span className="font-bold text-primary text-blue-600">50</span> newer desktops for terchnical classes to use for the upcoming school year.</li>
+              <li>Installed improved security software on new laptops and desktops to keep user information private and secure.</li>
               <li>Configured operating systems and software to optimize the student user experience.</li>
-              <li>Dissassembled damaged technology to salvage parts that could be reused.</li>
+              <li>Dissassembled damaged technology to salvage parts that could be reused for future use.</li>
             </ul>
           </div>
           <div className="bg-card p-6 rounded-lg shadow-md border">
@@ -74,7 +74,7 @@ export default function ExperiencePage() {
             <ul className="list-disc list-inside space-y-2 text-foreground">
               <li>Assisted in the grand opening of the new Fair Oaks store so customers and partners have a wonderful experience and minimize shopping times for customers.</li>
               <li>Managed cash register operations, processing transactions involving cash, checks, and credit cards.</li>
-              <li>trained new and current partners on inputting codes, restocking items, and tend to customers the H-E-B way.</li>
+              <li><span className="font-bold text-primary">Trained</span> new and current partners on inputting codes, restocking items, and tend to customers the H-E-B way.</li>
               <li>Ensuring that A+ Hospitality is always given when interacting with customers and partners.</li>
             </ul>
           </div>
