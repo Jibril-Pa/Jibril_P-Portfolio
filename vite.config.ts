b967fcf05db290d0f9bce27375b6dc115f5e949f
Jibril-Pa <109011292+Jibril-Pa@users.noreply.github.com>
2025-10-24 13:13:45 -0500

    PLEASE WORK

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3000,
+    host: true
+  },
   build: {
     rollupOptions: {
       external: ['tailwindcss/version.js']
